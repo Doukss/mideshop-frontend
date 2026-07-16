d3760fba9e377bc8d34493a51636c2a9d6cfad12
Malick Mbodji <dmbodji297@gmail.com>
2026-07-16 12:46:42 +0000

    Update footer text for consistency and clarity

--- src/App.jsx
+++ src/App.jsx
@@ -127,7 +127,7 @@ export default function App() {
         />
       </main>
 
-      <footer className="app-footer">MediShop- 221</footer>
+      <footer className="app-footer">MediShop-tp E221</footer>
     </div>
   );
 }
